5036b22b359b071c0ee1aa8bf244788ec792f789
vmcguire <victor.mcguire@gmail.com>
2021-03-02 20:32:54 -0800

    updated errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,23 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
+
 var getRepoIssues = function (repo) {
   var apiURL = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
   fetch(apiURL).then(function (response) {
@@ -15,7 +32,7 @@ var getRepoIssues = function (repo) {
         }
       });
     } else {
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
@@ -67,4 +84,4 @@ var displayWarning = function (repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
